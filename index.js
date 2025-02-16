098451bc54bbfc4ddbb1c57e753dd9bac3b343fe
Maeva Nono <maeva.nono@efrei.net>
2025-02-16 13:52:17 +0100

    Configuration de l'environnement docker et nodejs

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('🚀 API is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
