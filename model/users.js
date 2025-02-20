b8b1cdddb7aefc1184b28f8553c727c57fb82098
Maeva Nono <maeva.nono@efrei.net>
2025-02-20 09:31:06 +0100

    model user terminer

--- model/users.js
+++ model/users.js
@@ -5,4 +5,26 @@ class Users{
         const result = await pool.query('SELECT * FROM users');
         return result.rows
     }
+
+    static async getUserById(){
+        const result = await pool.query('SELECT * FROM users WHERE id = $1', [id])
+        return result.rows[0]
+    }
+
+    static async createUser({ nom, prenom, poids, taille, email, date_naissance }){
+        const result = await pool.query(
+            'INSERT INTO users (nom, prenom, poids, taille, email, date_naissance) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [nom, prenom, poids, taille, email, date_naissance]
+        )
+        return result.rows[0];
+    }
+
+    static async updateUser(id, {nom, prenom, poids, taille, email, date_naissance}){
+        const result = await pool.query(
+            'UPDATE users SET nom = $1, prenom = $2, poids = $3, taille = $4, email = $5, date_naissance = $6 WHERE id = $6 RETURNING *' [nom, prenom, poids, taille, email, date_naissance]
+        )
+    }
+
+    static async deleteUser(id){
+        await pool.query('DELETE FROM users WHERE id = $1', [id]);
+    }
 }
